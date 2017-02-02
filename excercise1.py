ec76ddf238c68b38aba581ff37ce3a26b8b19190
AlfredoMondragon <amondragon4@live.nmhu.edu>
2017-02-02 12:06:18 -0700

    First

---
+++ excercise1.py
@@ -0,0 +1,21 @@
+# git-exercise-01.py
+
+# TEAM LEADER:
+# implement this function so that it returns copy of string_arg reversed
+def reverseWord(string_arg):
+	def reverseWord(string_arg):
+	string_arg[::-1]
+
+# TEAM MEMBER:
+# implement this function so that it returns the frequency of query in string_arg
+def countFreq(string_arg, query):
+	pass
+
+def main():
+	data = 'guidorossumwashere'
+	print 'REVERSED ==>', reverseWord(data)
+	print 'FREQUENCY OF s IN', data, '==>', countFreq(data, 's')
+
+if __name__ == "__main__": 
+	main()
+
