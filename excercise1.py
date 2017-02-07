07a018af03f866f6caffe42c2f42f1bbe8359bdc
sje96 <stevenesquibel@hotmail.com>
2017-02-07 11:26:48 -0700

    Update excercise1.py

--- excercise1.py
+++ excercise1.py
@@ -8,7 +8,11 @@ def reverseWord(string_arg):
 # TEAM MEMBER:
 # implement this function so that it returns the frequency of query in string_arg
 def countFreq(string_arg, query):
-	pass
+	cout = 0
+	for i in string.arg:
+		if i == query:
+			count += 1
+	return count
 
 def main():
 	data = 'guidorossumwashere'
